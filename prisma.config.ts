4133cc5dd8d02844d24cb80250c940dbfa237b1e
Abhishek Mishra <akminator1999@gmail.com>
2026-07-14 17:57:58 +0530

    Fix datasource to singular in prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,27 +1,12 @@
 // prisma.config.ts
-import path from 'node:path'
 import { defineConfig } from 'prisma/config'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 export default defineConfig({
-  earlyAccess: true,
-  schema: path.join('prisma', 'schema.prisma'),
+  schema: 'prisma/schema.prisma',
   datasource: {
-    url: process.env.DIRECT_URL!,
-  },
-  migrations: {                    //  NSeeding configuration for migrations
-    seed: 'tsx ./prisma/seed.ts',
-  },
-
-  //URL to my postgres database, using the DIRECT_URL environment variable from .env
-  migrate: {
-    async adapter() {
-      const { PrismaPg } = await import('@prisma/adapter-pg')
-      return new PrismaPg({
-        connectionString: process.env.DIRECT_URL!,
-      })
-    },
+    url: process.env.DATABASE_URL,
   },
 })
